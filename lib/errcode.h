68c7357299b48e2d8664998c84fad358d4b3123d
Ruediger Haertel <r_haertel@gmx.de>
2020-11-02 11:33:55 +0100

    new error codes.

--- lib/errcode.h
+++ lib/errcode.h
@@ -40,15 +40,17 @@ enum {
 	kErrGeneric   = -1,
 
 	kErrBase               = 0x2000,
-	kErrUpperLimit        = (kErrBase + 1),
-	kErrLowerLimit        = (kErrBase + 2),
+	kErrUpperLimit        = (kErrBase +  1),
+	kErrLowerLimit        = (kErrBase +  2),
 
-	kErrNoVector          = (kErrBase + 3),
-	kErrInvalidChan       = (kErrBase + 4),
+	kErrNoVector          = (kErrBase +  3),
+	kErrInvalidChan       = (kErrBase +  4),
 
-	kErrAccessDenied      = (kErrBase + 5),
-	kErrInvalidValue      = (kErrBase + 6),
-	kErrInvalidType       = (kErrBase + 7),
+	kErrAccessDenied      = (kErrBase +  5),
+	kErrInvalidValue      = (kErrBase +  6),
+	kErrInvalidType       = (kErrBase +  7),
+	kErrSizeTooBig        = (kErrBase +  8),
+	kErrInvalidArg        = (kErrBase +  9),
 
 };
 
